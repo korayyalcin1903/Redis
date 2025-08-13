4f08989610ecf84354995cb4d8085ec588e8538e
korayyalcin1903 <korayyalcin1903@gmail.com>
2025-08-13 22:40:44 +0300

    .

---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,54 @@
+﻿using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                {"localhost", 6383 },
+                {"localhost", 6384 },
+                {"localhost", 6385 },
+            },
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+
+        static public async Task<IDatabase> RedisMaterDatabase()
+        {
+            ConnectionMultiplexer sentinelConnection = await ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+            System.Net.EndPoint masterEndPoint = null;
+            foreach (System.Net.EndPoint endpoint in sentinelConnection.GetEndPoints())
+            {
+                IServer server = sentinelConnection.GetServer(endpoint);
+                if (!server.IsConnected)
+                {
+                    continue;
+                }
+                masterEndPoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break;
+            }
+
+            var localMasterIp = masterEndPoint.ToString() switch
+            {
+                "172.19.0.2:6379" => "localhost:6379",
+                "172.19.0.3:6379" => "localhost:6380",
+                "172.19.0.4:6379" => "localhost:6381",
+                "172.19.0.5:6379" => "localhost:6382",
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIp);
+            IDatabase database = masterConnection.GetDatabase();
+            return database;
+        }
+    }
+}
